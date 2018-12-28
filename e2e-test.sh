11da4197263d9bf7d485938d7f0f2b3f38f632e7
Vesna Tan <tanvezi@gmail.com>
2018-12-28 17:55:45 -0500

    added #!/usr/bin/env bash

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,8 +1,6 @@
+#!/usr/bin/env bash
 source "~/.bashrc"
-# if [ "$#" -ne 2 ]; then
-#   echo "Usage: $0 $REACT_APP_MABL_API_KEY $REACT_APP_MABL_ENV_ID"
-#   exit 0
-# fi
+
 set -o nounset
 set -o errexit
 set -o pipefail
@@ -10,7 +8,7 @@ set -o pipefail
 if test -z "$TESTING_API"
 then
   echo "Empty"
-  exit 1
+  exit 0
 else 
    echo " WORKS! $TESTING_API"
 fi
