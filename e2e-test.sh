f52fa2f5da8599e06a19de7ee57b1355025bf3be
Vesna Tan <tanvezi@gmail.com>
2018-12-29 09:37:00 -0500

    testing e2esh

--- e2e-test.sh
+++ e2e-test.sh
@@ -3,11 +3,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# source ~/.bashrc
+echo $TESTING_API
 API = $TESTING_API
-if [ -z "$API" ];
+echo `${API}`
+if [ -z "$TESTING_API" ];
 then
   echo "Empty"
   exit 1
 else 
-   echo "WORKS! $API"
+   echo "WORKS! $TESTING_API"
 fi
