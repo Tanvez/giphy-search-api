9afb9559e5bbdbc38025f19bd270702591896799
Vesna Tan <tanvezi@gmail.com>
2018-12-28 19:45:37 -0500

    removed source

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-source "~/.bashrc"
-
+TESTING_API = ${TESTING_API}
 set -o nounset
 set -o errexit
 set -o pipefail
 
-if test -z "$TESTING_API"
+if test -z TESTING_API
 then
   echo "Empty"
   exit 0
 else 
-   echo " WORKS! $TESTING_API"
+   echo " WORKS! " + TESTING_API
 fi
+
+cat ~/.bashrc
