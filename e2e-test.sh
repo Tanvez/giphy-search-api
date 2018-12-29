146ebaeb3ee5108d0a19327d38ba6f96d4dfc376
Vesna Tan <tanvezi@gmail.com>
2018-12-29 09:12:14 -0500

    testing sh6

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-TESTING_API = ${TESTING_API}
 set -o nounset
 set -o errexit
 set -o pipefail
 
+TESTING_API = ${TESTING_API}
 if [ -z "$TESTING_API"]
 then
   echo "Empty"
   exit 1
 else 
-   echo " WORKS! " + TESTING_API
+   echo "WORKS! $TESTING_API"
 fi
 
 cat ~/.bashrc
