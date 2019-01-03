d950077bccfa1f20141e2cd9a3e55d1a91f970b6
Vesna Tan <tanvezi@gmail.com>
2019-01-03 16:55:44 -0500

    refactor2

--- e2e-test.sh
+++ e2e-test.sh
@@ -7,12 +7,12 @@
 if [ "${CIRCLE_BRANCH}" == "dev" ];
 then
   # echo $DEV_API
-  TESTING_API =$DEV_API
+  TESTING_API=$DEV_API
   
 elif [ "${CIRCLE_BRANCH}" == "master" ];
 then
   # echo $PROD_API
-  TESTING_API =$PROD_API
+  TESTING_API=$PROD_API
 fi
 
 echo $TESTING_API
